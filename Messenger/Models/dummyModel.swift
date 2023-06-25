228c3e732245308892f268e6dbe5f67253a6103f
Mallikharjun Kakarla <mallikharjun.kakarla@teamlease.com>
2023-06-25 11:20:33 +0530

    Wrapped up initial setUp

---
+++ Messenger/Models/dummyModel.swift
@@ -0,0 +1,8 @@
+//
+//  dummyModel.swift
+//  Messenger
+//
+//  Created by Mallikharjun kakarla on 25/06/23.
+//
+
+import Foundation
---
+++ Messenger/ViewModels/dummyVM.swift
@@ -0,0 +1,8 @@
+//
+//  dummyVM.swift
+//  Messenger
+//
+//  Created by Mallikharjun kakarla on 25/06/23.
+//
+
+import Foundation
